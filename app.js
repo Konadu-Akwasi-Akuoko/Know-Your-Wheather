9c3ec606998fb2bbbfe81fd73d4c829a46700faf
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2022-08-22 13:58:49 +0000

    Changed the port to Process.env.port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const https = require("https");
 const { configurationKey } = require("./config");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const apiKey = configurationKey.apiKey;
 const apiURLGeoLocation = "https://api.openweathermap.org/geo/1.0/direct";
