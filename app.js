88a503992118914868980233e15ba05fe5e3c794
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2022-08-23 22:39:49 +0000

    Fixed the time and date, added a loading animation screen

--- app.js
+++ app.js
@@ -29,45 +29,64 @@ app.get("/weather", (reqExpress, resExpress) => {
   //First get the location data from the API, then get the weather data from the API.
   //All inside the first HTTPSget method.
 
-  https.get(
-    `${apiURLGeoLocation}?appid=${apiKey}&limit=1&q=${reqExpress.query.LocName}`,
-    (resHTTPS) => {
-      resHTTPS
-        .on("data", (data) => {
-          let parsedData = JSON.parse(data);
-          //? Strip down the data to lon and lat, and call the callback with it.
-          parsedData = { lat: parsedData[0].lat, lon: parsedData[0].lon };
-          // callBack(parsedData);
-          let latitude = parsedData.lat;
-          let longitude = parsedData.lon;
-          https.get(
-            `${apiWeather}?lat=${latitude}&lon=${longitude}&units=${apiUnits}&appid=${apiKey}`,
-            (resHTTPS2) => {
-              resHTTPS2.on("data", (data) => {
-                let parsedData = JSON.parse(data);
-                console.log(parsedData);
-                temperature = parsedData.main.temp;
-                windSpeed = parsedData.wind.speed;
-                humidity = parsedData.main.humidity;
-                weather = parsedData.weather[0].description;
-                let JSONdata = {
-                  temperature: temperature,
-                  windSpeed: windSpeed,
-                  humidity: humidity,
-                  weather: weather,
-                };
-                JSON.stringify(JSONdata);
-                resExpress.send(JSONdata);
-                console.log("Finished sending data");
+  try {
+    https.get(
+      `${apiURLGeoLocation}?appid=${apiKey}&limit=1&q=${reqExpress.query.LocName}`,
+      (resHTTPS) => {
+        resHTTPS
+          .on("data", (data) => {
+            try {
+              let parsedData = JSON.parse(data);
+              //? Strip down the data to lon and lat, and call the callback with it.
+              parsedData = { lat: parsedData[0].lat, lon: parsedData[0].lon };
+              // callBack(parsedData);
+              let latitude = parsedData.lat;
+              let longitude = parsedData.lon;
+              https.get(
+                `${apiWeather}?lat=${latitude}&lon=${longitude}&units=${apiUnits}&appid=${apiKey}`,
+                (resHTTPS2) => {
+                  resHTTPS2.on("data", (data) => {
+                    let parsedData = JSON.parse(data);
+                    console.log(parsedData);
+                    temperature = parsedData.main.temp;
+                    windSpeed = parsedData.wind.speed;
+                    humidity = parsedData.main.humidity;
+                    weather = parsedData.weather[0].description;
+                    let JSONdata = {
+                      temperature: temperature,
+                      windSpeed: windSpeed,
+                      humidity: humidity,
+                      weather: weather,
+                    };
+                    JSON.stringify(JSONdata);
+                    resExpress.send(JSONdata);
+                    console.log("Finished sending data");
+                  });
+                }
+              );
+            } catch (e) {
+              console.log(e);
+              resExpress.send({
+                temperature: "N/A",
+                windSpeed: "N/A",
+                humidity: "N/A",
+                weather: "/A",
               });
             }
-          );
-        })
-        .on("error", (err) => {
-          console.log(`Error: ${err}`);
-        });
-    }
-  );
+          })
+          .on("error", (err) => {
+            console.log(`Error: ${err}`);
+          });
+      }
+    );
+  } catch (e) {
+    resExpress.send({
+      temperature: "N/A",
+      windSpeed: "N/A",
+      humidity: "N/A",
+      weather: "/A",
+    });
+  }
 });
 
 app.listen(port, () => {
--- public/Scripts/index.js
+++ public/Scripts/index.js
@@ -1,10 +1,19 @@
+$(document).ready(function () {
+  getWeather();
+  $("#dateText").html(getDate);
+  $("#timeText").html(getTime);
+});
+
 function getWeather() {
   console.log("IndexJS getWeather message: " + $("#search-box").val());
-  //
+  $(".loader").addClass("showLoader");
+  $(".loader").removeClass("removeLoader");
   fetch(`/weather?LocName=${$("#search-box").val()}`)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      $(".loader").remove("showLoader");
+      $(".loader").addClass("removeLoader");
       $("#locationDP").html(`${$("#search-box").val()}`);
       $("#bigTempDP").html(`${data.temperature}<sup>oc</sup>`);
       $("#smallTempDP").html(`${data.temperature}<sup>oc</sup>`);
@@ -12,4 +21,22 @@ function getWeather() {
       $("#smallHumidityDP").html(`${data.humidity}%`);
       $("#smallWeatherDP").html(`${data.weather}`);
     });
+}
+
+//Getting the date in MM/DD/YYYY
+function getDate() {
+  let date = new Date();
+  return (
+    date.getDate() +
+    "/" +
+    Number(date.getMonth() + 1).toString() +
+    "/" +
+    +date.getFullYear()
+  );
+}
+
+//Getting the time in hh/mm
+function getTime() {
+  let date = new Date();
+  return date.getHours() + ":" + date.getMinutes();
 }
